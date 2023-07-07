459d1fdc9caf4e7071533c7f25f1cd97357487a1
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2023-07-07 14:41:32 -0300

    Include Test Integration

--- src/test/java/com/porto/devcatalog/Porto/controllers/ProductControllerTests.java
+++ src/test/java/com/porto/devcatalog/Porto/controllers/ProductControllerTests.java
@@ -1,4 +1,50 @@
 package com.porto.devcatalog.Porto.controllers;
 
+import com.porto.devcatalog.Porto.DTO.ProductDTO;
+import com.porto.devcatalog.Porto.services.ProductService;
+import com.porto.devcatalog.Porto.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
 public class ProductControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+
+        when(productService.findAllProducts(any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+        ResultActions result = mockMvc.perform(get("/products")
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+
+    }
 }
