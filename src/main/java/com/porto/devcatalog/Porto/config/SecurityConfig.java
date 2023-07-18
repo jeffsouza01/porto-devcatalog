579e38bc8fd41ae28613415a12c82cecc7dbe8bd
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2023-07-18 16:45:21 -0300

    Update Security Config to H2 COnsole

--- src/main/java/com/porto/devcatalog/Porto/config/SecurityConfig.java
+++ src/main/java/com/porto/devcatalog/Porto/config/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +15,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().
-                requestMatchers("/**").permitAll();
-        
 
-        return http.build();
+        return http.authorizeHttpRequests(
+                authorizeConfig -> {
+                    authorizeConfig.requestMatchers("/**").permitAll();
+                    authorizeConfig.anyRequest().authenticated();
+
+                }).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()).disable())
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .build();
     }
 }
---
+++ src/main/java/com/porto/devcatalog/Porto/controllers/UserController.java
@@ -0,0 +1,58 @@
+package com.porto.devcatalog.Porto.controllers;
+
+import com.porto.devcatalog.Porto.DTO.UserCreateDTO;
+import com.porto.devcatalog.Porto.DTO.UserDTO;
+import com.porto.devcatalog.Porto.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public Page<UserDTO> findAllUsers(Pageable page){
+
+        return userService.findAllUsers(page);
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO findUserById(@PathVariable Long id){
+        return userService.findUserById(id);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insertNewUser(@RequestBody UserCreateDTO dto) {
+        UserDTO newDto = userService.createUser(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(newDto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO dto) {
+        dto = userService.updateUser(id, dto);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+}
